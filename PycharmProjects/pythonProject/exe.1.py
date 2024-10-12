00939468335ef297b135c0eeeb5484a24339eea8
imfamou <santiagopodebatista@gmail.com>
2024-10-12 12:26:42 +0100

    Add files via upload

---
+++ PycharmProjects/pythonProject/exe.1.py
@@ -0,0 +1,35 @@
+#print("ola") #eu tambem posso comentar aqui
+#se eu fizer uma função print, a consola vai escrever aquilo que pus nos parêntesis
+
+
+#------------------ COMENTARIOS ---------------------
+# o comentario é uma parte do codigo que não vaiser lida pelo programa ,ou seja:
+# podemos escrever anotações no codigo sem que elas perturbem o seu funcionamento
+# devido à SINTAXE do python
+# PYTHON -> linguagem de programação
+#                 |
+#                 v
+#             gramàtica --> SINTAXE, ou seja, como a limguagem FUNCIONA
+
+# IMPORTANTE: SEMPRE que queremos comentar alguma coisa, temos de colocar [#] ANTES e em TODAS AS LINHAS DE COMENTÁRIO
+
+palavra= "hamburger" # MAIS DOIS TIPOS DE DADOS: VARIÁVEL (que armazena um dado); e STRING (texto)
+# quando temos uma STRING podemos "copiar e colar" o que está dentro de aspas
+
+print("hamburger")
+print(palavra) #podemos usar as variáveis (texto branco) em qualquer parte do código
+
+#-------------------------------VARIÁVEIS---------------------------------------
+
+# uma variável contém um valor que pode mudar, como se fosse uma caixa em que podemos pôr várias coisas (e alterá-las)
+#REGRAS:
+# NÃO PODEM começar com números!
+
+print(5) #número fica AZUL
+# podemos fazer contas com NÚMEROS
+# se uma VARIÁVEL começar com um NÚMERO, o código vai AUTOMATICAMENTE pensar que estamos a tratar de um NÚMERO:
+
+porcento=5 #NO ENTANTO, podemos definir uma VARIÁVEL com apenas um número
+# isto
+# isto acontece porque estamos a armazenar o valor de 5 na variável, ou seja, PORCENTO=5, MAS 5 NÃO PODE SER IGUAL A
+#                                                                                                      PORCENTO
