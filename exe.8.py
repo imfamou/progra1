f456b151d51544d70a436ecd92c2bfd33fae3556
imfamou <santiagopodebatista@gmail.com>
2024-11-09 11:43:33 +0000

    Add files via upload

---
+++ exe.8.py
@@ -0,0 +1,6 @@
+y= int(input("Escolhe um número: "))
+
+x= y-1 # com esta operação, calculamos o valor do número que vem antes do y (ou seja, y-1)
+z= y+1 # aqui, descobrimos o valor do número que vem depois do y
+
+print("O número que vem antes do escolhido é: ",x,"e o número que vem depois é: ",z)
